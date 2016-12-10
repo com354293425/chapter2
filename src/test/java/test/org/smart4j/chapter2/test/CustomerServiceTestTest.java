12063c5e38d679fe22951e15441c51cebe34f87d
com_zhang <w354293425@gmail.com>
2016-12-10 19:28:15 +0800

    first

---
+++ src/test/java/test/org/smart4j/chapter2/test/CustomerServiceTestTest.java
@@ -0,0 +1,85 @@
+package test.org.smart4j.chapter2.test; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* CustomerServiceTest Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Ê®¶þÔÂ 8, 2016</pre> 
+* @version 1.0 
+*/ 
+public class CustomerServiceTestTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: init() 
+* 
+*/ 
+@Test
+public void testInit() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCustomerListTest() 
+* 
+*/ 
+@Test
+public void testGetCustomerListTest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCustomerTest() 
+* 
+*/ 
+@Test
+public void testGetCustomerTest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: creatCustomerTest() 
+* 
+*/ 
+@Test
+public void testCreatCustomerTest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: updateCustomerTest() 
+* 
+*/ 
+@Test
+public void testUpdateCustomerTest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: deleteCustomerTest() 
+* 
+*/ 
+@Test
+public void testDeleteCustomerTest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
---
+++ src/test/resources/sql/customer_init.sql
@@ -0,0 +1,3 @@
+TRUNCATE customer;
+INSERT into customer (name, contact, telephone, email, remark) VALUES ('customer1', 'Jack', '18234085323', 'Jack@gmail.com', null);
+INSERT into customer (name, contact, telephone, email, remark) VALUES ('customer2', 'Rose', '15736876570', 'Rose@gmail.com', null);
